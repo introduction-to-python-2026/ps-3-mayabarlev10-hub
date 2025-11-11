26ced964862141fe95228e58c306589e3c96045c
mayabarlev10-hub <maya.barlev10@gmail.com>
2025-11-11 22:45:40 +0200

    Update edge_case_exercise.py

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -1,12 +1,15 @@
 def move(my_list, direction):
-    index_of_one = my_list.index(1)
+    try:
+        index_of_one = my_list.index(1)
+    except ValueError:
+        return my_list # מחזיר את הרשימה אם 1 לא נמצא
 
-    if direction == 'right' and index_of_one < len(my_list) -1:
-        my_list[index_of_one] = 0
-        my_list[index_of_one + 1] = 1
+    if direction == 'right' and index_of_one < len(my_list) - 1:
+        my_list[index_of_one] = 0        # הופך את המיקום הנוכחי ל-0
+        my_list[index_of_one + 1] = 1    # הופך את המיקום החדש (ימינה) ל-1
 
     elif direction == 'left' and index_of_one > 0:
-        my_list[index_of_one] = 0
-        my_list[index_of_one - 1] = 1
+        my_list[index_of_one] = 0        # הופך את המיקום הנוכחי ל-0
+        my_list[index_of_one - 1] = 1    # הופך את המיקום החדש (שמאלה) ל-1
 
     return my_list
